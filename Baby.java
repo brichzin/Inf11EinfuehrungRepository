f302894d0fb818657c7a7f106a315d07be791f7b
Cornelius <CorneliusX7@yahoo.com>
2020-05-07 13:59:33 +0200

    getter-Methoden bei Klasse "Baby" ergänzt

--- Baby.java
+++ Baby.java
@@ -47,4 +47,14 @@ public class Baby implements Audio
             return weinen();
         }
     }
+    
+    public int AlterGeben()
+    {
+    return alterinMonaten;
+    }
+    
+    public int AufregungGeben()
+    {
+    return aufregungsstufe;
+    }
 }
