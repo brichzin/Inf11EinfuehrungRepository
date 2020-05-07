1cdc033fa2d49d9dcafb065afaa3a9478a033296
yannick2003 <willi.wilhelm9@gmail.com>
2020-05-07 14:12:38 +0200

    Alter und Gewicht editiert

--- ELEFANT.java
+++ ELEFANT.java
@@ -10,15 +10,15 @@ public class ELEFANT
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
      int alter;
      int gewicht;
-
+     
     /**
      * Konstruktor für Objekte der Klasse Elefant
      */
     public ELEFANT (int alterNeu, int gewichtNeu )
     {
         
-       alter = alterNeu;
-       gewicht = gewichtNeu;
+       alter = 187;
+       gewicht = 69;
    
     } 
     public String lautGeben()
